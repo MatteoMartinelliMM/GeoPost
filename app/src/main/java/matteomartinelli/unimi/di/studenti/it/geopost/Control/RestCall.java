8be5dd9b3fff58d5d71ccfdbb924dbd8b45d122f
utente2.academy <matteomartinelli1b@gmail.com>
2017-12-19 08:56:22 -0800

    Creata loginActivity Creata SplashScreenActivity Creata OverviewActivity Creata LoginActivity Bozza di Model

--- app/src/main/java/matteomartinelli/unimi/di/studenti/it/geopost/Control/RestCall.java
+++ app/src/main/java/matteomartinelli/unimi/di/studenti/it/geopost/Control/RestCall.java
@@ -2,6 +2,7 @@ package matteomartinelli.unimi.di.studenti.it.geopost.Control;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
 /**
  * Created by utente2.academy on 12/19/2017.
@@ -11,12 +12,12 @@ public class RestCall {
     private static final String BASE_URL = "https://ewserver.di.unimi.it/mobicomp/geopost";
     private static AsyncHttpClient client = new AsyncHttpClient();
 
-    public static void get(String url, AsyncHttpResponseHandler asyncHttpResponseHandler) {
-        client.get(getAbsoluteUrl(url), null, asyncHttpResponseHandler);
+    public static void get(String url, RequestParams params ,AsyncHttpResponseHandler asyncHttpResponseHandler) {
+        client.get(getAbsoluteUrl(url), params, asyncHttpResponseHandler);
     }
 
-    public static void post(String url, AsyncHttpResponseHandler asyncHttpResponseHandler) {
-        client.post(getAbsoluteUrl(url), null, asyncHttpResponseHandler);
+    public static void post(String url,RequestParams params, AsyncHttpResponseHandler asyncHttpResponseHandler) {
+        client.post(getAbsoluteUrl(url), params, asyncHttpResponseHandler);
     }
 
     private static String getAbsoluteUrl(String relativeUrl) {
